b0e168d0cbaeefbc869b7f2555b97654c817b3ac
Paul Paguay <ulvierps@hotmail.com>
2018-06-28 00:33:15 -0500

    serverpython3

--- fechas.py
+++ fechas.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-
+import time
 ahora = datetime.now()  # Obtiene fecha y hora actual
 
 print("Microsegundos:",ahora.microsecond)  # Muestra microsegundo
+
+milli_sec = int(round(time.time() * 1000))
+print(milli_sec)
--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import pygame.camera
 import sys
 import time
 
-from datetime import datetime
 
 port = 5000
 pygame.init()
@@ -18,20 +17,20 @@ serversocket.bind(("",port))
 serversocket.listen(1)
 
 
-tiempo_1=ahora.microsecond #Inicio encendido de camara
+tiempo_1=int(round(time.time() * 1000)) #Inicio encendido de camara
 pygame.camera.init()
 webcam = pygame.camera.Camera("/dev/video0",(320,240))
 webcam.start()
-tiempo_2=ahora.microsecond  #Fin encedido de camara
+tiempo_2=int(round(time.time() * 1000))  #Fin encedido de camara
 while True:
 
         connection, address = serversocket.accept()
-        tiempo_3=ahora.microsecond #Inicio captura imagen
+        tiempo_3=int(round(time.time() * 1000)) #Inicio captura imagen
         image = webcam.get_image() # capture image
         data = pygame.image.tostring(image,"RGB") # convert captured image to string, use RGB color scheme
-        tiempo_4=ahora.microsecond #Fin captura imagen  inicio envio
+        tiempo_4=int(round(time.time() * 1000)) #Fin captura imagen  inicio envio
         connection.sendall(data)
-        tiempo_5=ahora.microsecond #Fin envio
+        tiempo_5=int(round(time.time() * 1000)) #Fin envio
         f.write("%d,%d,%d,%d,%d\r\n" %(tiempo_1,tiempo_2,tiempo_3,tiempo_4,tiempo_5))
      	
         time.sleep(0.1)
